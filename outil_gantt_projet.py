93ffc997c4e0cf0cdf046d2772a6c6358ab65443
benjaminlallemand44 <benjamin.lallemand44@gmail.com>
2025-11-30 23:41:28 +0100

    Update outil_gantt_projet.py

--- outil_gantt_projet.py
+++ outil_gantt_projet.py
@@ -5,21 +5,10 @@ from datetime import timedelta
 import streamlit.components.v1 as components
 
 # URL brute du logo sur GitHub
-svg_url = "https://github.com/benjaminlallemand44/Projet_renovation_Gantt/blob/main/images/Logo_ACTEE_CMYN.svg"
+logo_url = "https://github.com/benjaminlallemand44/Projet_renovation_Gantt/blob/main/images/Logo_ACTEE_CMYN-HD.png"
 
-response = requests.get(svg_url)
-svg_content = response.text
-
-# Afficher le SVG avec st.markdown
-st.markdown(
-    f"""
-    <div style="width: 150px; height: auto;">
-        {svg_content}
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
+# Afficher le logo
+st.image(logo_url, width=150)
     
 # --------------------
 # Glossaire simplifié pour affichage dans les inputs (tooltips)
